6e9accd051d01e6315689ad17ee6738c5fb0eca7
Nick Wallingford <nick@wallingford.cc>
2017-03-09 15:20:19 -0800

    Added a few more evaluators.

--- evaluator.cpp
+++ evaluator.cpp
@@ -18,16 +18,40 @@ namespace eval {
  * They must also be within namespace eval{}
  */
 
-int eval_black_pyramid(const array<unsigned, 4> &pieces,
-                       char player __attribute__((unused)), int weight) {
+int eval_black_dyke(const array<unsigned, 4> &pieces, char, int weight) {
+  int retval = __builtin_popcount(pieces[0] & 0x4223);
+  retval *= retval * weight;
+  retval /= 25;
+  return retval;
+}
+
+int eval_white_dyke(const array<unsigned, 4> &pieces, char, int weight) {
+  int retval = __builtin_popcount(pieces[1] & 0xc4420000u);
+  retval *= retval * -weight;
+  retval /= 25;
+  return retval;
+}
+
+int eval_d_diagonal(const array<unsigned, 4> &pieces, char, int weight) {
+  int black = __builtin_popcount(pieces[0] & 0x11224488);
+  black *= black * weight;
+  black /= 64;
+
+  int white = __builtin_popcount(pieces[0] & 0x11224488);
+  white *= white * weight;
+  white /= 64;
+
+  return black - white;
+}
+
+int eval_black_pyramid(const array<unsigned, 4> &pieces, char, int weight) {
   int retval = __builtin_popcount(pieces[0] & 0x267);
   retval *= retval * weight;
   retval /= 36;
   return retval;
 }
 
-int eval_white_pyramid(const array<unsigned, 4> &pieces,
-                       char player __attribute__((unused)), int weight) {
+int eval_white_pyramid(const array<unsigned, 4> &pieces, char, int weight) {
   int retval = __builtin_popcount(pieces[1] & 0xe6400000u);
   retval *= retval * -weight;
   retval /= 36;
@@ -41,8 +65,7 @@ int eval_centralized_kings(const array<unsigned, 4> &pieces,
   return weight * retval;
 }
 
-int eval_trapped_kings(const array<unsigned, 4> &pieces, char player,
-                       int) {
+int eval_trapped_kings(const array<unsigned, 4> &pieces, char player, int) {
   return 0;
 
   int retval = 0;
